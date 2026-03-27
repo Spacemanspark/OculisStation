77790c2baef7bc0abb12a7fa55e71339870185f5
Lucy <lucy@absolucy.moe>
2026-03-27 12:16:35 -0400

    adds a character pref to opt-out of being chosen for the "Employee Birthday" station trait (#147)

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/oculis/birthday_opt_out.tsx
@@ -0,0 +1,9 @@
+// THIS IS A OCULIS UI FILE
+import { CheckboxInput, type FeatureToggle } from '../../base';
+
+export const birthday_opt_out: FeatureToggle = {
+  name: 'Opt-Out Of Random Birthdays',
+  description:
+    'If selected, you will not be selected by the "Employee Birthday" station trait.',
+  component: CheckboxInput,
+};
